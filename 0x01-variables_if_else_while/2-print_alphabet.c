e78653dae8a100fabb9d3c304b04ed97a121d0eb
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-12 08:05:12 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main-is the main function that printts all alphabets in small letters
+ *
+ * Return:returns always zero
+*/
+int main(void)
+{
+	for (int c = 97 ; c <= 122 ; c++)
+	{
+		putchar(c);
+	}
+return (0);
+}
