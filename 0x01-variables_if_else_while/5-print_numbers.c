12bda692df287172380e3f13e3c743dd04fe3529
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-12 09:28:32 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-is the main function that printts all alphabets in small letters
+ *
+ * Return:returns always zero
+ */
+int main(void)
+{
+int c = 0;
+for (; c <= 9; c++)
+{
+printf("%d", c);
+}
+return (0);
+}
