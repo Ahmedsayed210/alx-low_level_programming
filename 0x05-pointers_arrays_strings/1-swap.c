6112071b31fe28b4b0cc169201771f9db0cf2ad2
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-21 15:20:01 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * swap_int -  swaps the values of two integers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * return = 0 success
+*/
+void swap_int(int *a, int *b)
+{
+a = a - b;
+b = a + b;
+a = b - a;
+return (0);
+}
