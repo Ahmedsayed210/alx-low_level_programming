4400764c5fbfce2969b006a9e84a67107fed6926
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-21 19:59:08 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element of an array of integers
+ *
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+int ahmed[n]{*a};
+printf("%d\n", ahmed[n]);
+return (0);
+}
