356b6dca4f2745501c35488399b3f5e4af89e09f
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-21 17:14:22 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _puts -  prints a string, followed by a new line
+ *
+ * @str: string parameter to print
+ * return = 0 success
+*/
+void _puts(char *str)
+{
+char ahmed[] = "I do not fear computers. I fear the lack of them";
+_putchar(ahmed);
+return (0);
+}
