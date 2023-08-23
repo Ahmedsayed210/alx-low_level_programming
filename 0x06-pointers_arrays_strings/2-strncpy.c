bd01fca21f5bfa62677257492c7e07085a10b7d5
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-23 08:46:35 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _strncpy - *_strncpy
+ *
+ * @dest: char to check
+ * @src: char to check
+ * @n: int to check
+ *
+ * Return: char
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+while (i < n)
+dest[i] = '\0';
+i++;
+return (dest);
+}
