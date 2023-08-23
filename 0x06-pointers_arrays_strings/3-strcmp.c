09f9600cbadcd418b46bed1d61d5ef11ca82e9ce
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-23 09:12:49 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: char to check
+ * @s2: char to check
+ *
+ * Return: char
+*/
+int _strcmp(char *s1, char *s2)
+{
+strcmp(*s1, *s2)
+return (0);
+}
