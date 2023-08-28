10bca914922868f889b183ffb25a13eb6fe469c9
Ahmedsayed210 <romaahmed791@gmail.com>
2023-08-28 09:53:24 -0700

    task8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagsums - prints the sums of the two diagonals of a square matrix
+ * @a: pointer to start of matrix
+ * @size: width of matrix column
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int ahmed, menna, doaa, l = 0, r = 0;
+
+	for (ahmed = 0; ahmed < size; ahmed++)
+	{
+		doaa = (ahmed * size) + ahmed;
+		l += *(a + doaa);
+	}
+	for (menna = 0; menna < size; menna++)
+	{
+		p = (menna * size) + (size - 1 - menna);
+		r += *(a + doaa);
+	}
+	printf("%i, %i\n", l, r);
+}
