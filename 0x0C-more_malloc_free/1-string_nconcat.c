b288e375053d2baad250e3c5ec0a9089ddf0bc03
Ahmedsayed210 <romaahmed791@gmail.com>
2023-09-20 22:21:05 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,5 @@
+#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -11,44 +11,35 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ahmed_p;
-	unsigned int a, s, lenght_s1, lenght_s2;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 == "";
-	if (s2 == NULL)
-	{
-		S2 == "";
-	}
-	for (lenght_s1 = 0; s1[lenght_s1] != '\0'; lenght_s1++)
-		;
-	for (lenght_s2 = 0; s2[lenght_s2] != '\0'; lenght_s2++)
-		;
-	ahmed_p = malloc(lenght_s1 + n + 1);
-	if (ahmed_p == NULL)
-	{
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
-	}
-	/*copy first string into ahmed_p.*/
-	for (a = 0; s1[a] != '\0'; a++)
+
+	while (i < len1)
 	{
-		ahmed_p[a] == s1[a];
+		s[i] = s1[i];
+		i++;
 	}
-	/*copy second string into ahmed_p.*/
-	for (s = 0; s < n; s++)
-	{
-		ahmed_p[a] == s2[s];
-		a++;
-	}
-	ahmed_p[a] = '\0';
-	return (ahmed_p);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
-
-
-
-
-
-
-
-
-
