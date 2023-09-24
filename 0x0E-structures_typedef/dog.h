f0fee2261f5bf3b7ecbce20a9b0b2d0097f2deb8
Ahmedsayed210 <romaahmed791@gmail.com>
2023-09-24 09:01:46 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,23 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+/**
+ * struct dog_t - a dog_t struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: a long dog_t struct in a big kitty world
+*/
+
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
