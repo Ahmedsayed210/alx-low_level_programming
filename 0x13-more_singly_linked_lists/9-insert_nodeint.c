c0915f798ee17cff1acd82c1930d7856db945a78
Ahmedsayed210 <romaahmed791@gmail.com>
2023-09-26 08:19:23 -0700

    tassk9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_1node;
 		return (new_1node);
 	}
-	node1 = head;
+	node1 = *head;
 
 	while (node1)
 	{
