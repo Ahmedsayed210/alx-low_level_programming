af39b5f167a61781feffb16c41a6ae2c7f4602a2
Ahmedsayed210 <romaahmed791@gmail.com>
2023-09-28 05:35:01 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: pointer to intger
+*/
+
+void print_binary(unsigned long int n)
+{
+	int bit = size of(n) * 8, printed_num = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar ('1');
+			printed_num++;
+		}
+		else if (printed_num)
+			_putchar ('0');
+	}
+	if (!printed_num)
+		_putchar ('0');
+}
