67c8207a12bd81cbd82df3299d2f0a7d6f3ff82b
Ahmedsayed210 <romaahmed791@gmail.com>
2023-11-09 14:31:59 -0800

    task0

--- 0x17-doubly_linked_lists/dlists.h
+++ 0x17-doubly_linked_lists/dlists.h
@@ -3,8 +3,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: points to integer
+ * @prev: points to pointer
+ * @next: points to pointer
+*/
+typedef struct dlistint_s
+{
+	struct dlistint_t *prev;
+	int n;
+	struct dlistint_t *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 
 
-#endif "DLISTS_H"
+
+
+
+
+#endif /*DLISTS_H*/
