be662386a6bba9812158ebd3e9da37ae324e779c
Ahmedsayed210 <romaahmed791@gmail.com>
2024-01-24 05:06:00 -0800

    C - Makefiles

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,19 @@
+#include "m.h"
+/**
+ * main - Entry point of the program, invokes the print_school function
+ *
+ * Description:
+ * This function serves as the starting point for the program. It calls the
+ * print_school function, which is expected to handle the main logic of the
+ * program. The purpose of the program is determined by the behavior
+ * implemented within the print_school function.
+ *
+ * Return:
+ * The function always returns EXIT_SUCCESS, indicating successful execution
+ * of the program.
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
