8c099532c76941f341b15aeef74f0cfeab90adf8
Ahmedsayed210 <romaahmed791@gmail.com>
2024-03-16 17:22:04 -0700

    1-task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -43,6 +43,7 @@ typedef struct skiplist_s
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
